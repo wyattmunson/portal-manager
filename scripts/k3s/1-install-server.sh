93e82f5c56cff154f134ac59cf7eac7a9cbcc07d
Wyatt <munsonwf@gmail.com>
2023-07-27 11:30:32 -0700

    feat: add k3s server and agent deployment

---
+++ scripts/k3s/1-install-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Installing k3s server..."
+
+curl -sfL https://get.k3s.io | sh -
---
+++ scripts/k3s/2-install-agent.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Installing k3s agent..."
+
+sudo k3s agent --server https://192.168.1.19:6443
