fb544b34a0da4d41f07694a7946f25fa28c9e542
Wyatt Munson <munsonwf@gmail.com>
2024-01-27 23:15:30 -0800

    fix: update seed script

--- seed.sh
+++ seed.sh
@@ -12,7 +12,18 @@ echo //////////////////////////////////
 echo // Installing tenji seed script //
 echo //////////////////////////////////
 
+echo "Moving to home directory"
+cd ~
+
 echo "Tenji will install the required dependencies. Enter your password when promted."
 
 sudo apt update
+sudo apt install python -y
+sudo apt install git -y
 
+# install ansible
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+
+git clone https://github.com/wyattmunson/portal-manager
